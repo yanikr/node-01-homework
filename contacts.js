19d9056b7c1352643b92815a8a35dc23d756a799
Yanik Rikhter <rihter.yan@gmail.com>
2022-12-24 10:54:38 +0200

    fix

--- contacts.js
+++ contacts.js
@@ -13,9 +13,6 @@ async function listContacts() {
 async function getContactById(contactId) {
   const contacts = await listContacts();
   const result = contacts.find((contact) => contact.id === contactId);
-  if (!result) {
-    return null;
-  }
   return result;
 }
 
